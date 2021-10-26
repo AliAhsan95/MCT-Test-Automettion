b57cb61686a4286cba0dcd057b7b5483c46d7bef
AliAhsan95 <aliahsandreams@gmail.com>
2021-10-25 21:21:30 -0400

    updated report style

--- src/test/java/ARunner/RunnerTest.java
+++ src/test/java/ARunner/RunnerTest.java
@@ -10,12 +10,18 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 @CucumberOptions(
 		features = "src/test/resources/",
 		glue={"stepdefinationcode"},
-		plugin = { "pretty", "html:target/test-reports/cucumber-reports.html" },
-		//tags = "@wip",
+						plugin = { "pretty",
+
+						"html:target/cucumber-reports/cucumber-reports.html",
+						"html:target/cucumber-reports/cucumber-pretty",
+						"json:target/cucumber-reports/CucumberTestReport.json"
+				},
+				
+		//tags = "@sc02",
 		monochrome=true,
 		snippets = SnippetType.CAMELCASE
 		)
 
-public class runcucumbeTest {
+public class RunnerTest {
 
 }
